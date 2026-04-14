77abfc30933c17417c54c671a0afa9d9d4f95e80
SonamTenzin7 <sonamtenzey0@gmail.com>
2026-04-14 11:06:47 +0600

    image size fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -58,6 +58,7 @@ export default defineConfig(async (): Promise<UserConfig> => {
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
